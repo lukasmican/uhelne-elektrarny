ef66e479bcb3189e4c3ad8e2b395b92488709690
Lukáš Mičan <micek.luki@seznam.cz>
2023-12-06 23:02:21 +0100

    Fixed routing on github pages

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -4,5 +4,6 @@ ng build --configuration production --aot --base-href https://lukasmican.github.
 
 rm -r docs
 mv dist/hvt-uhelne-elektrarny/ docs/
+cp docs/index.html docs/404.html
 
 echo "Compilation done and files are in correct folders, push changes to git to complete the operation."
